3bf1e14a8695444cb163e2e95fb916ea76ad7268
Guilherme Melo <guilherme96melo@gmail.com>
2024-09-25 21:01:26 +0200

    Inicio en C# uno de los lenguages requeridos en el curso de Ilerna con ayuda de Alura. Problemas de configuraciñón solucionados. Se usa el terminal para asegurar que el programa enseña los simbolos especiales con dotnet run Programa.cs

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+string mensajeDeBienvenida= "Bienvenidos a Screen Sound";
+void MostrarMensajeDeBienvenida()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    Console.WriteLine(mensajeDeBienvenida);
+}
+void MostrarMenu()
+{
+    Console.WriteLine("\nElija 1 para registrar una banda");
+    Console.WriteLine("Elija 2 para listar todas las bandas");
+    Console.WriteLine("Elija 3 para puntuar una banda");
+    Console.WriteLine("Elija 4 para ver la media de una banda");
+    Console.WriteLine("Elija -1 para salir");
+    Console.Write("\nElige la opción: ");
+    string opciónElegida = Console.ReadLine()!;//! evita el valor nulo
+    int opciónElegidaNumerica = int.Parse(opciónElegida);
+    
+    switch(opciónElegidaNumerica)
+    {
+        case 1: Console.WriteLine("Elegiste la opción " + opciónElegidaNumerica);
+            break;
+        case 2: Console.WriteLine("Elegiste la opción " + opciónElegidaNumerica);
+            break;
+        case 3: Console.WriteLine("Elegiste la opción " + opciónElegidaNumerica);
+            break;
+        case 4: Console.WriteLine("Elegiste la opción " + opciónElegidaNumerica);
+            break;
+        case -1: Console.WriteLine("Elegiste la opción " + opciónElegidaNumerica);
+            break;
+        default: Console.WriteLine("Opción no válida");
+            break;
+    }
+}
+MostrarMensajeDeBienvenida();
+MostrarMenu();
+
+
+
